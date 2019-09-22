7c023ef0c8e8e680a87b0f5bc7a9c0f0f73e5daf
julesjoules <julia.walker1998@gmail.com>
2019-09-22 12:22:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,16 @@ $verb = $_SERVER["REQUEST_METHOD"];
 if($verb == "GET"){
   echo "GET HERE REQ";
 } else if($verb == "POST"){
-  echo "OH YEA POSTMAN";
+  //set constants
+  $author = "anonymous";
+  $content = "shakespeare was a fake";
+  echo "OH YEA POSTMAN";//default  message
+  if(isset($_POST["author"])){
+    $author = $_POST["author"];
+  }
+   if(isset($_POST["content"])){
+    $constent = $_POST["content"];
+   }
 } else {
   echo "USAGE GET or POST";
 }
