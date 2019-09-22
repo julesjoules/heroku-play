cf9310cc36ce3598f61a474a09f6b492d366eb9e
julesjoules <julia.walker1998@gmail.com>
2019-09-22 12:33:06 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php>
 //open localhost url
+  //Starter guid to post and get requests for databases
 print_r($_POST);
 $verb = $_SERVER["REQUEST_METHOD"];
 
@@ -7,8 +8,8 @@ if($verb == "GET"){
   echo "GET HERE REQ";
 } else if($verb == "POST"){
   //set constants
-  $author = "anonymous";
-  $content = "shakespeare was a fake";
+  $author = "anonymous";//postman
+  $content = "shakespeare was a fake";///ooOOoooOoOOoo yeaaa
   echo n12br("OH YEA POSTMAN \r\n");//default  message, n12br is for new line breaks
   if(isset($_POST["author"])){
     $author = $_POST["author"];
