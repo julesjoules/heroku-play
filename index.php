eab39233566ce6ec8318398d506b6a26a8900566
julesjoules <julia.walker1998@gmail.com>
2019-09-22 12:12:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php>
 //open localhost url
 print_r($_POST);
+$verb = $_SERVER["REQUEST_METHOD"];
+
+if($verb == "GET"){
+  echo "GET HERE REQ";
+} else if($verb == "POST"){
+  echo "OH YEA POSTMAN";
+} else {
+  echo "USAGE GET or POST";
+}
 
 ?>
