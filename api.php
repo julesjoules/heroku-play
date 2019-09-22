80d5e1e5283816585fcd8eb3579ae4160be00306
julesjoules <julia.walker1998@gmail.com>
2019-09-22 17:57:32 -0400

    Update api.php

--- api.php
+++ api.php
@@ -3,12 +3,41 @@
     //this is the basic way of getting a database handler from PDO, PHP's built in quasi-ORM
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
+    $count = 1;
+    $binvar = decbin($count);
+
+    $reciever = $_GET['reciever'];
+
+    $rack = $reciever;
+    $sizeRack = strlen($rack);
+
+    $finalRack = array();
+    $outcount = 0;
+
+    //while loop to check for all words associated
+    while(strlen(decbin($count)) < $sizeRack + 10{
+        $binvar = decbin($count);
+        $binvar = strrev($binvar);
+        $count++;
+        $tempArr = "";
+        for($x=0; $x< strlen($binvar); $x++){
+            if($binvar[$x] == "1"){
+                $tempArr = $tempArr.$rack[$x];
+            }
+        }
+        
+        $query = "SELECT words FROM racks WHERE rack = \"$tmpArr\"";
+        //$statement = $dbhanfle->prepare($query);
+        //$statement->execute();
+        //$results = $statemebt->fetchA
  
+
     //this is a sample query which gets some data, the order by part shuffles the results
     //the limit 0, 10 takes the first 10 results.
     // you might want to consider taking more results, implementing "pagination", 
     // ordering by rank, etc.
-    $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 10";
+//figure out how to do reset/ get new button to start game and load rack of words
+    //$query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 10";
     
     //this next line could actually be used to provide user_given input to the query to 
     //avoid SQL injection attacks
@@ -20,6 +49,24 @@
     //I chose to get associative arrays inside of a big array
     //this will naturally create a pleasant array of JSON data when I echo in a couple lines
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    //echo jason_encode($results);
+//else, if not new button pressed, if its the check button, than cross check the words
+    //set up rack and variables needed to return all combinations of words
+    if($results == array()){
+    }
+    else{
+           $finalResults = array();
+           foreach($results[0] as $values){
+            $finalResults = $values;
+         }
+    $explodArr = explode('@@', $finalResults);
+    for($y = 0;$y<count($explodArr); $y++){
+        array_ush($finalRack, $explodArr[$y]);
+    }
+}
+    }
+          
+        
     
     //this part is perhaps overkill but I wanted to set the HTTP headers and status code
     //making to this line means everything was great with this request
